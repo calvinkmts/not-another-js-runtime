4eacbaa039a87f889c80d1396631ae6363303311
Calvin Kamtoso <calvinkmts@gmail.com>
2025-10-21 19:30:08 +0700

    feat: update ops_fs.rs

--- runtime-core/src/ops_fs.rs
+++ runtime-core/src/ops_fs.rs
@@ -1,30 +1,20 @@
-use deno_core::{Extension, op2};
-
-pub fn ext_fs() -> Extension {
-    Extension::builder("najsr_fs")
-        .ops(vec![
-            op_read_file::decl(),
-            op_write_file::decl(),
-            op_remove_file::decl(),
-        ])
-        .build()
-}
+use deno_core::op2;
 
 #[op2(async)]
 #[string]
-async fn op_read_file(#[string] path: String) -> Result<String, std::io::Error> {
+pub async fn op_read_file(#[string] path: String) -> Result<String, std::io::Error> {
     tokio::fs::read_to_string(path).await
 }
 
 #[op2(async)]
-async fn op_write_file(
+pub async fn op_write_file(
     #[string] path: String,
     #[string] content: String,
 ) -> Result<(), std::io::Error> {
-    tokio::fs::write(path, contents).await
+    tokio::fs::write(path, content).await
 }
 
 #[op2(fast)]
-fn op_remove_file(#[string] path: String) -> Result<(), std::io::Error> {
-    tokio::fs::remove_file(path)
+pub fn op_remove_file(#[string] path: String) -> Result<(), std::io::Error> {
+    std::fs::remove_file(path)
 }
